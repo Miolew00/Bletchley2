ead4cd6a0599e23e30a26711c32a735b7afce95b
Magika34 <mirokolev059@gmail.com>
2026-04-30 13:17:11 +0300

    .

--- Bletchley2.0/Models/Guess.cs
+++ Bletchley2.0/Models/Guess.cs
@@ -8,6 +8,6 @@
         public string GuessCode { get; set; } = string.Empty;
         public int KnownNumbers { get; set; }
         public int KnownPositions { get; set; }
-        public int AttemptNumber { get; set; }
+        public int AttemptNumber { get; set; } // This will help us track the order of guesses for each game
     }
 }
