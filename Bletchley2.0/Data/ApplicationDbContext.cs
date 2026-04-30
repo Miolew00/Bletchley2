931550b18f4606bcf57456483e8d38d6d135b184
Miolew22 <maggiana282008@gmail.com>
2026-04-30 12:26:04 +0300

    Add project files.

---
+++ Bletchley2.0/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bletchley2._0.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Bletchley2.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bletchley2._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
